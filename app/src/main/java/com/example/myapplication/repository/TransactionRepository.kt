73f3278ab633d0d2c01465c75ee2da67c9a06ddf
naufal <89512179+nomsf@users.noreply.github.com>
2024-04-05 13:38:05 +0700

    feat: coroutine for dao access

--- app/src/main/java/com/example/myapplication/repository/TransactionRepository.kt
+++ app/src/main/java/com/example/myapplication/repository/TransactionRepository.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.MutableLiveData
 import com.example.myapplication.dao.TransactionDao
 import com.example.myapplication.database.TransactionDatabase
 import com.example.myapplication.entities.TransactionEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 
 class TransactionRepository(context: Context) {
@@ -18,17 +20,29 @@ class TransactionRepository(context: Context) {
         allTransactions = transactionDao.getAllTransactions()
     }
 
-    fun insertTransaction(title: String, nominal: String, kategori: String, lokasi: String, tanggal: String) {
-        val transaction = TransactionEntity(title = title, nominal = nominal, kategori = kategori, lokasi = lokasi, tanggal = tanggal)
-        transactionDao.insertTransaction(transaction)
+    suspend fun insertTransaction(title: String, nominal: String, kategori: String, lokasi: String, tanggal: String){
+        withContext(Dispatchers.IO) {
+            val transaction = TransactionEntity(
+                title = title,
+                nominal = nominal,
+                kategori = kategori,
+                lokasi = lokasi,
+                tanggal = tanggal
+            )
+            transactionDao.insertTransaction(transaction)
+        }
     }
 
-    fun updateTransaction(title: String, nominal: String, kategori: String, id: Int) {
-        // id buat yang mau diupdate data yang mana
-        transactionDao.update(title, nominal, kategori, id)
+    suspend fun updateTransaction(title: String, nominal: String, kategori: String, id: Int) {
+        withContext(Dispatchers.IO) {
+            // id buat yang mau diupdate data yang mana
+            transactionDao.update(title, nominal, kategori, id)
+        }
     }
 
-    fun deleteTransaction(id: Int) {
-        transactionDao.delete(id)
+    suspend fun deleteTransaction(id: Int) {
+        withContext(Dispatchers.IO) {
+            transactionDao.delete(id)
+        }
     }
 }
