9fada5cd60293c451b9f8f95d1c06defea237245
naufal <89512179+nomsf@users.noreply.github.com>
2024-04-05 15:16:53 +0700

    feat: scan dialog and send to server

--- app/src/main/java/com/example/myapplication/ui/scan/ScanFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/scan/ScanFragment.kt
@@ -2,11 +2,13 @@ package com.example.myapplication.ui.scan
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.content.ActivityNotFoundException
 import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.graphics.ImageFormat
 import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraCaptureSession
@@ -25,12 +27,19 @@ import android.view.Surface
 import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentScanBinding
+import com.example.myapplication.entities.TransactionEntity
 import com.example.myapplication.repository.AuthRepository
+import com.example.myapplication.repository.TransactionRepository
 import com.example.myapplication.util.SecretPreference
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -55,8 +64,10 @@ class ScanFragment : Fragment() {
     private var cameraPermission: Boolean = false
     private lateinit var fileImage: File
     private lateinit var imagePath : String
+    private lateinit var imageByteArray : ByteArray
 
     private lateinit var authRepository : AuthRepository
+    private lateinit var transactionRepository: TransactionRepository
 
     private val stateCallBack = object : CameraDevice.StateCallback() {
         override fun onOpened(camera: CameraDevice) {
@@ -95,6 +106,24 @@ class ScanFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         authRepository = AuthRepository(SecretPreference(requireContext()))
+        transactionRepository = TransactionRepository(requireContext())
+
+        authRepository.billResponseLiveData.observe(this, Observer {
+            for (item in it.items) {
+                Log.i("Development", "Item: ${item.name}")
+                val transaction = TransactionEntity(
+                    title = item.name,
+                    nominal = item.price,
+                    kategori = "Bill_Scan",
+                    lokasi = null,
+                    tanggal = null
+                )
+
+                lifecycleScope.launch {
+                    transactionRepository.insertTransaction(transaction)
+                }
+            }
+        })
     }
 
     override fun onCreateView(
@@ -109,11 +138,38 @@ class ScanFragment : Fragment() {
 
         cameraPermissionCheck()
 
-        binding.buttonScan.setOnClickListener {
-            takePicture()
+        // preparing laert dialog for image preview
+        val builder = AlertDialog.Builder(requireContext())
+        val dialogInflater = layoutInflater
+        val dialogLayout = dialogInflater.inflate(R.layout.dialog_scan_confirmation, null)
+        val buttonRetake = dialogLayout.findViewById<Button>(R.id.retake_button_dialog)
+        val buttonSave = dialogLayout.findViewById<Button>(R.id.save_button_dialog)
+
+        // creating imageReader for the camera
+        val imageReader = ImageReader.newInstance(290, 420, ImageFormat.JPEG, 1)
+
+        imageReader.setOnImageAvailableListener({ reader ->
+            Log.i("Development", "image listener called")
+            reader.acquireNextImage().use { image ->
+                val buffer = image.planes[0].buffer
+                val bytes = ByteArray(buffer.remaining())
+                imageByteArray = bytes
+                buffer.get(bytes)
+//                saveImage(bytes)
+                val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                val imageView = dialogLayout.findViewById<ImageView>(R.id.bill_image)
+
+                imageView.setImageBitmap(bitmap)
+            }
+        }, null)
+
+        val alertDialog = builder.create()
+        buttonRetake.setOnClickListener {
+            alertDialog.dismiss()
         }
 
-        binding.buttonSend.setOnClickListener {
+        buttonSave.setOnClickListener {
+            saveImage(imageByteArray)
             if(::imagePath.isInitialized) {
                 CoroutineScope(Dispatchers.IO).launch {
                     val uri = Uri.parse(imagePath)
@@ -130,7 +186,34 @@ class ScanFragment : Fragment() {
             } else {
                 Log.w("Development", "Image path not initialized")
             }
+
+            alertDialog.dismiss()
         }
+
+        binding.buttonScan.setOnClickListener {
+            takePicture(imageReader)
+            alertDialog.setView(dialogLayout)
+            alertDialog.show()
+        }
+
+//        binding.buttonSend.setOnClickListener {
+//            if(::imagePath.isInitialized) {
+//                CoroutineScope(Dispatchers.IO).launch {
+//                    val uri = Uri.parse(imagePath)
+//                    val parcelFileDescriptor = requireContext().contentResolver.openFileDescriptor(uri, "r")
+//                    val file = File.createTempFile("upload", null, requireContext().cacheDir)
+//                    val inputStream = FileInputStream(parcelFileDescriptor?.fileDescriptor)
+//                    val outputStream = FileOutputStream(file)
+//                    inputStream.copyTo(outputStream)
+//
+//                    authRepository.uploadBillRequest(file)
+//
+//                    parcelFileDescriptor?.close()
+//                }
+//            } else {
+//                Log.w("Development", "Image path not initialized")
+//            }
+//        }
 
         return binding.root
     }
@@ -242,30 +325,26 @@ class ScanFragment : Fragment() {
     }
 
 
-    private fun takePicture() {
+    private fun takePicture(imageReader: ImageReader) {
         val captureRequestBuilderTake = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE)
-        val imageReader = ImageReader.newInstance(290, 420, ImageFormat.JPEG, 1)
+//        val imageReader = ImageReader.newInstance(290, 420, ImageFormat.JPEG, 1)
 
-        imageReader.setOnImageAvailableListener({ reader ->
-            Log.i("Development", "image listener called")
-            reader.acquireNextImage().use { image ->
-                val buffer = image.planes[0].buffer
-                val bytes = ByteArray(buffer.remaining())
-                buffer.get(bytes)
-                saveImage(bytes)
-                Log.i("Development", "Image saved")
-            }
-        }, null)
-
-
+//        imageReader.setOnImageAvailableListener({ reader ->
+//            Log.i("Development", "image listener called")
+//            reader.acquireNextImage().use { image ->
+//                val buffer = image.planes[0].buffer
+//                val bytes = ByteArray(buffer.remaining())
+//                buffer.get(bytes)
+//                saveImage(bytes)
+//                Log.i("Development", "Image saved")
+//            }
+//        }, null)
         captureRequestBuilderTake.addTarget(imageReader.surface)
-        captureRequestBuilderTake.addTarget(Surface(binding.cameraPreview.surfaceTexture))
 
-        val outputConfig = OutputConfiguration(Surface(binding.cameraPreview.surfaceTexture))
         val outputConfigReader = OutputConfiguration(imageReader.surface)
 
         val sessionConfig = SessionConfiguration(SessionConfiguration.SESSION_REGULAR,
-            listOf(outputConfig, outputConfigReader), ContextCompat.getMainExecutor(requireContext()), object : CameraCaptureSession.StateCallback() {
+            listOf(outputConfigReader), ContextCompat.getMainExecutor(requireContext()), object : CameraCaptureSession.StateCallback() {
                 override fun onConfigured(session: CameraCaptureSession) {
                     session.capture(captureRequestBuilderTake.build(), null, null)
                 }
